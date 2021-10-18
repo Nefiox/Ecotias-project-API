3df7546ce163769e4a5a6bff07ba2fc0f2fe9711
Juan Jiménez <nefioxdev@gmail.com>
2021-10-17 23:02:26 -0500

    Debugging v4

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 
 // API routes
 const productsRoutesAPI = require("./routes/productsRoutesAPI");
 const usersRoutesAPI = require("./routes/usersRoutesAPI");
 
 const cors = require("cors");
-const logger = require('morgan');
+const logger = require("morgan");
 
 // App
 const app = express();
@@ -20,11 +20,13 @@ app.use(express.json()); // JSON Parser
 app.use(cors());
 
 // Main route
-app.get('/', (req, res) => res.send('okkkk'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views/index.html"))
+);
 
 // API routes
-// app.use("/api/usuarios", usersRoutesAPI); // Users
-// app.use("/api/productos", productsRoutesAPI); // Products
+app.use("/api/usuarios", usersRoutesAPI); // Users
+app.use("/api/productos", productsRoutesAPI); // Products
 
 // Server
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
